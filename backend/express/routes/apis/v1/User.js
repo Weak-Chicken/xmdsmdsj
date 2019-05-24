6b388c056d2cbc69346d27037452613409b829ae
Jon Xie <Weak-Chicken@users.noreply.github.com>
2019-05-24 16:16:32 +0800

    support post to use form-data

--- backend/express/routes/apis/v1/User.js
+++ backend/express/routes/apis/v1/User.js
@@ -1,18 +1,20 @@
 // Use express module
 var express = require('express');
+var multipart = require('connect-multiparty');
 var router = express.Router();
 
 // Use SQL modules with our configs
 var mysql = require('mysql');
 var mysqlConfig = require('../../../db/sql/sqlConfigs');
 var mysqlUserOp = require('../../../db/sql/userSqlOp');
+var multipartMiddleware = multipart();
 
 // Build a connection pool for sql connection
 var mysqlPool = mysql.createPool(mysqlConfig.mysql);
 
 // User login
 // DO NOT use 'get' here. Use 'post' to secure user's password.
-router.post('/Login', (req, res, next) => {
+router.post('/Login', multipartMiddleware, (req, res, next) => {
   // Get connection from connection pool
   // mysqlPool.getConnection((err, connection) => {
   //   if (err) throw (err);
