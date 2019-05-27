6816f58fa7644fd6fe412a24c5235f15aa0dc324
Jon Xie <Weak-Chicken@users.noreply.github.com>
2019-05-27 11:31:09 +0800

    add communication support functions

--- backend/express/routes/apis/v1/User.js
+++ backend/express/routes/apis/v1/User.js
@@ -8,6 +8,7 @@ let mysql = require('mysql');
 let mysqlConfig = require('../../../db/sql/sqlConfigs');
 let mysqlUserOp = require('../../../db/sql/userSqlOp');
 let flags = require('../../__flags__');
+let supportCommunicationMethods = require('./__communicationSupport__');
 
 let multipartMiddleware = multipart();
 let flagCode = flags.flags();
@@ -15,21 +16,15 @@ let flagCode = flags.flags();
 // Build a connection pool for sql connection
 let mysqlPool = mysql.createPool(mysqlConfig.mysql);
 
+
 // User login
 // DO NOT use 'get' here. Use 'post' to secure user's password.
 router.post('/login', multipartMiddleware, (req, res, next) => {
-  if (req.session.logIn) {
-    res.status(400);
-    res.send({
-      'success': false,
-      'flag': flagCode.ERROR_ALREADY_LOGGED_IN,
-    });
-    return;
-  }
+  if (!supportCommunicationMethods.checkLogin('Login', req, res, next)) return;
 
   // Get connection from connection pool
   mysqlPool.getConnection((err, connection) => {
-    if (err) throw (err);
+    if (!supportCommunicationMethods.checkSQLConnection(err, mysqlPool, connection, flagCode.ERROR_UNKNOWN_SQL_CONNECTION_ERROR)) return;
 
     let [
       uuid,
@@ -42,25 +37,10 @@ router.post('/login', multipartMiddleware, (req, res, next) => {
       ];
 
     connection.query(mysqlUserOp.getUserById, [uuid], (error, results, fields) => {
-      if (error) {
-        console.log(error);
-        res.send({
-          'success': false,
-          'flag': flagCode.ERROR_UNKNOWN_USER_LOGIN_ERROR,
-          'error': error,
-        });
-        return;
-      }
+      if (!supportCommunicationMethods.checkSQLConnection(error, mysqlPool, connection, flagCode.ERROR_UNKNOWN_USER_LOGIN_ERROR)) return;
       
-      // res.send({
-      //   'results': results
-      // });
-
-      // console.log(results);
-      // console.log(results.uuid);
-      // console.log(results[0]);
-      // console.log(results[0].uuid);
       if (results.length === 0) { // TODO: improve here!
+        res.status(400);
         res.send({
           'success': false,
           'flag': flagCode.ERROR_USER_NOT_FOUND
@@ -69,69 +49,56 @@ router.post('/login', multipartMiddleware, (req, res, next) => {
       }
 
       results = results[0]
-      // console.log([uuid,userName,userPwd]);
+      let sentData;
+
       if ((results.uuid == uuid) && (results.userName == userName) && (results.userPwd == userPwd)) {
         req.session.logIn = true;
         req.session.logInUser = results.uuid;
 
-        res.send({
+        sentData = {
           'success': true,
           'flag': flagCode.INFO_USER_LOGIN_SUCCEEDED,
           'userData': results,
-        });
+        };
       } else {
+        res.status(400);
         if (results.uuid != uuid) {
-          res.send({
+          sentData = {
             'success': false,
             'flag': flagCode.ERROR_USER_UUId_WRONG
-          });
-          // console.log('expected:', results.uuid, typeof(results.uuid))
-          // console.log('received:', uuid, typeof(uuid))
-          return;
-        }
-        if (results.userName != userName) {
-          res.send({
+          };
+        } else if (results.userName != userName) {
+          sentData = {
             'success': false,
             'flag': flagCode.ERROR_USER_NAME_WRONG
-          });
-          return;
-        }
-        if (results.userPwd != userPwd) {
-          res.send({
+          };
+        } else if (results.userPwd != userPwd) {
+          sentData = {
             'success': false,
             'flag': flagCode.ERROR_USER_PASSWORD_WRONG
-          });
-          return;
+          };
         } else {
-          res.send({
+          sentData = {
             'success': false,
             'flag': flagCode.ERROR_USER_PASSWORD_WRONG
-          });
-          return;
+          };
         }
       }
-      });
-    // Release the connection
-    // connection.release(); // might not work
-    mysqlPool.releaseConnection(connection);
+
+      supportCommunicationMethods.sendAndCloseConnection(res, mysqlPool, connection, sentData);
+    });
   });
 });
+
 
 // User register
 // DO NOT use 'get' here. Use 'post' to secure user's password.
 router.post('/register', multipartMiddleware, (req, res, next) => {
-  if (req.session.logIn) {
-    res.status(400);
-    res.send({
-      'success': false,
-      'flag': flagCode.ERROR_ALREADY_LOGGED_IN,
-    });
-    return;
-  }
+  if (!supportCommunicationMethods.checkLogin('Login', req, res, next)) return;
 
   // Get connection from connection pool
   mysqlPool.getConnection((err, connection) => {
-    if (err) throw (err);
+    if (!supportCommunicationMethods.checkSQLConnection(err, mysqlPool, connection, flagCode.ERROR_UNKNOWN_USER_LOGIN_ERROR)) return;
 
     let [
       uuid,
@@ -146,90 +113,49 @@ router.post('/register', multipartMiddleware, (req, res, next) => {
       ];
 
     connection.query(mysqlUserOp.insertSimplfied, [uuid, userName, userPwd, userEmail], (error, results, fields) => {
-      if (error) {
-        console.log(error);
-        res.send({
-          'success': false,
-          'flag': flagCode.ERROR_UNKNOWN_USER_REGISTER_ERROR,
-          'error': error,
-        });
-        return;
-      };
+      if (!supportCommunicationMethods.checkSQLConnection(error, mysqlPool, connection, flagCode.ERROR_UNKNOWN_USER_LOGIN_ERROR)) return;
 
       res.send({
         'success': true,
         'flag': flagCode.INFO_USER_REGISTER_SUCCEEDED,
       });
-
+      // Release the connection
+      // connection.release(); // might not work
+      mysqlPool.releaseConnection(connection);
     });
-    // Release the connection
-    // connection.release(); // might not work
-    mysqlPool.releaseConnection(connection);
   });
 });
 
+
 router.post('/getUserById', multipartMiddleware, (req, res, next) => {
-  if (!(req.session.logIn)) {
-    res.status(400);
-    res.send({
-      'success': false,
-      'flag': flagCode.ERROR_NOT_LOGGED_IN,
-    });
-    return;
-  }
+  if (!supportCommunicationMethods.checkLogin('NotLogin', req, res, next)) return;
 
   let uuid = parseInt(req.body.uuid);
 
   mysqlPool.getConnection((err, connection) => {
-    if (err) {
-      console.log(err)
-    }
+    if (!supportCommunicationMethods.checkSQLConnection(err, mysqlPool, connection, flagCode.ERROR_UNKNOWN_SQL_CONNECTION_ERROR)) return;
 
     connection.query(mysqlUserOp.getUserByIdNormal, [uuid], (error, results, fields) => {
-      if (error) {
-        console.log(error);
-        res.send({
-          'success': false,
-          'flag': flagCode.ERROR_UNKNOWN_USER_LOGIN_ERROR,
-          'error': error,
-        });
-        return;
-      };
+      if (!supportCommunicationMethods.checkSQLConnection(error, mysqlPool, connection, flagCode.ERROR_UNKNOWN_SQL_CONNECTION_ERROR)) return;
 
       res.send({
         'success': true,
         'userData': results,
       });
+      mysqlPool.releaseConnection(connection);
     });
-  mysqlPool.releaseConnection(connection);
   });
 });
 
+
 router.post('/listUsers', multipartMiddleware, (req, res, next) => {
-  if (!(req.session.logIn)) {
-    res.status(400);
-    res.send({
-      'success': false,
-      'flag': flagCode.ERROR_NOT_LOGGED_IN,
-    });
-    return;
-  }
+  if (!supportCommunicationMethods.checkLogin('NotLogin', req, res, next)) return;
 
   mysqlPool.getConnection((err, connection) => {
-    if (err) {
-      console.log(err)
-    }
+    if (!supportCommunicationMethods.checkSQLConnection(err, mysqlPool, connection, flagCode.ERROR_UNKNOWN_SQL_CONNECTION_ERROR)) return;
 
     connection.query(mysqlUserOp.queryAllNormal, (error, results, fields) => {
-      if (error) {
-        console.log(error);
-        res.send({
-          'success': false,
-          'flag': flagCode.ERROR_UNKNOWN_USER_REGISTER_ERROR,
-          'error': error,
-        });
-        return;
-      };
+      if (!supportCommunicationMethods.checkSQLConnection(error, mysqlPool, connection, flagCode.ERROR_UNKNOWN_SQL_CONNECTION_ERROR)) return;
 
       res.send({
         'success': true,
@@ -241,45 +167,20 @@ router.post('/listUsers', multipartMiddleware, (req, res, next) => {
 });
 
 router.post('/getUserDetailsById', multipartMiddleware, (req, res, next) => {
-  if (!(req.session.logIn)) {
-    res.status(400);
-    res.send({
-      'success': false,
-      'flag': flagCode.ERROR_NOT_LOGGED_IN,
-    });
-    return;
-  }
+  if (!supportCommunicationMethods.checkLogin('NotLogin', req, res, next)) return;
 
   mysqlPool.getConnection((err, connection) => {
-    if (err) {
-      console.log(err)
-    }
+    if (!supportCommunicationMethods.checkSQLConnection(err, mysqlPool, connection, flagCode.ERROR_UNKNOWN_SQL_CONNECTION_ERROR)) return;
 
     let userData;
     connection.query(mysqlUserOp.getUserById, uuid, (error, results, fields) => {
-      if (error) {
-        console.log(error);
-        res.send({
-          'success': false,
-          'flag': flagCode.ERROR_UNKNOWN_USER_REGISTER_ERROR,
-          'error': error,
-        });
-        return;
-      };
+      if (!supportCommunicationMethods.checkSQLConnection(error, mysqlPool, connection, flagCode.ERROR_UNKNOWN_SQL_CONNECTION_ERROR)) return;
 
       // userData = results
     });
 
     connection.query(mysqlUserOp.getUserByIdNormal, uuid, (error, results, fields) => {
-      if (error) {
-        console.log(error);
-        res.send({
-          'success': false,
-          'flag': flagCode.ERROR_UNKNOWN_USER_REGISTER_ERROR,
-          'error': error,
-        });
-        return;
-      };
+      if (!supportCommunicationMethods.checkSQLConnection(error, mysqlPool, connection, flagCode.ERROR_UNKNOWN_SQL_CONNECTION_ERROR)) return;
 
       res.send({
         'success': true,
@@ -291,30 +192,13 @@ router.post('/getUserDetailsById', multipartMiddleware, (req, res, next) => {
 });
 
 router.post('/listUsersDetails', multipartMiddleware, (req, res, next) => {
-  if (!(req.session.logIn)) {
-    res.status(400);
-    res.send({
-      'success': false,
-      'flag': flagCode.ERROR_NOT_LOGGED_IN,
-    });
-    return;
-  }
+  if (!supportCommunicationMethods.checkLogin('NotLogin', req, res, next)) return;
 
   mysqlPool.getConnection((err, connection) => {
-    if (err) {
-      console.log(err)
-    }
+    if (!supportCommunicationMethods.checkSQLConnection(err, mysqlPool, connection, flagCode.ERROR_UNKNOWN_SQL_CONNECTION_ERROR)) return;
 
     connection.query(mysqlUserOp.getUserByIdNormal, uuid, (error, results, fields) => {
-      if (error) {
-        console.log(error);
-        res.send({
-          'success': false,
-          'flag': flagCode.ERROR_UNKNOWN_USER_REGISTER_ERROR,
-          'error': error,
-        });
-        return;
-      };
+      if (!supportCommunicationMethods.checkSQLConnection(error, mysqlPool, connection, flagCode.ERROR_UNKNOWN_SQL_CONNECTION_ERROR)) return;
 
       res.send({
         'success': true,
