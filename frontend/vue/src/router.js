7d02299ffe957823755422205cc54a9d12c385e4
Jon Xie <Weak-Chicken@users.noreply.github.com>
2019-06-07 17:59:01 +0800

    working on frontend logging in

--- frontend/vue/src/router.js
+++ frontend/vue/src/router.js
@@ -3,42 +3,57 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: '/',
       name: 'homepage',
-      component: () => import('@/views/PageHome.vue')
+      component: () => import('@/views/PageHome.vue'),
     },
     {
       path: '/about',
       name: 'aboutpage',
-      component: () => import('@/views/PageAbout.vue')
+      component: () => import('@/views/PageAbout.vue'),
     },
     {
       path: '/article',
       name: 'postpage',
-      component: () => import('@/views/PagePost.vue')
+      component: () => import('@/views/PagePost.vue'),
     },
     {
       path: '/article/:articleId',
       name: 'articlepage',
-      component: () => import('@/views/PageArticle.vue')
+      component: () => import('@/views/PageArticle.vue'),
     },
     {
       path: '/upcoming',
       name: 'upcomingpage',
-      component: () => import('@/views/PageUpComing.vue')
+      component: () => import('@/views/PageUpComing.vue'),
+      meta: {
+        requireAuthentication: true,
+      },
     },
     {
       path: '/login',
       name: 'authenticationpage',
-      component: () => import('@/views/PageAuthentication.vue')
+      component: () => import('@/views/PageAuthentication.vue'),
     },
     {
       path: '/register',
       name: 'authenticationpage',
-      component: () => import('@/views/PageAuthentication.vue')
+      component: () => import('@/views/PageAuthentication.vue'),
     },
   ]
-})
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requireAuthentication) {
+    alert('Ass we can');
+    next();
+  } else {
+    // If the page does not need authentication, let user in
+    next();
+  }
+});
+
+export default router;
