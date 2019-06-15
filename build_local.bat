d4e14c4b3bec48a68eedd3748da142fbc9e2cada
Jon Xie <Weak-Chicken@users.noreply.github.com>
2019-06-15 21:30:28 +0800

    buildlocal script for Windows

--- build_local.bat
+++ build_local.bat
@@ -1,4 +1,4 @@
-cd frontend/vue
+cd "./frontend/vue"
 
 REM cmd /k "npm run buildlocal"
 npm run buildlocal
@@ -8,6 +8,14 @@ cd ..
 
 dir
 
-rd /s /Q
+rd /s /Q "./Client/dist/"
+md "./Client/dist/"
+
+xcopy "./frontend/vue/dist" "./Client/dist" /s /e /y
+
+rd /s /Q "./Server/server/express/"
+md "./Server/server/express/"
+
+xcopy "./backend/express" "./Server/server/express" /s /e /y
 
 pause
